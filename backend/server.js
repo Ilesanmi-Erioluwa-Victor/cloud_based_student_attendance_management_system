a698adf68a87a5a396595972bfc8d97ee0e151b9
Ilesanmi Erioluwa Victor <ilesanmierioluwavictor@gmail.com>
2026-07-24 10:52:36 +0100

    Remove CORS origin restriction, allow all origins

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:3000', credentials: true }));
+app.use(cors());
 app.use(express.json());
 
 const authLimiter = rateLimit({
